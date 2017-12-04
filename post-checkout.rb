c411aaa071da7dddd372a7e3b61e239812a522dd
earendil95 <ermak95@gmail.com>
2017-12-04 12:00:52 +0300

    [#1] fix @palkan review

--- post-checkout.rb
+++ post-checkout.rb
@@ -3,20 +3,14 @@
 heads = '.git/refs/heads/'
 issues = '.git/refs/issues/'
 
-Dir.new(heads).each do |f|
-  next if f.match? /^\.+$/ # skipping '.' and '..'
+branch = File.read('.git/HEAD').match /^ref: refs\/heads\/(.+)/
 
-  # finding current branch name
-  commit = File.read(heads + f).chomp
-  next if commit != ARGV[1]
-
-  if File.exists?(issues + f)
-    issue = File.read issues + f
-    puts "Current issue is ##{issue}"
-    File.write '.git/ISSUE', issue
-  else
-    puts "Current issue is not specified yet"
-  end
+if File.exists?(issues + branch[1])
+  issue = File.read issues + branch[1]
+  puts "Current issue is ##{issue}"
+  File.write '.git/ISSUE', issue
+else
+  puts "Current issue is not specified yet"
 end
 
 exit(0)
--- prepare-commit-msg.rb
+++ prepare-commit-msg.rb
@@ -4,24 +4,24 @@ require 'fileutils'
 
 def cache_issue(number)
   File.write '.git/ISSUE', number
-  m = File.read('.git/HEAD').match /^ref: refs\/heads\/(.+)/
+  branch = File.read('.git/HEAD').match /^ref: refs\/heads\/(.+)/
   FileUtils.mkdir_p '.git/refs/issues'
-  File.write ".git/refs/issues/#{m[1]}", number if m
+  File.write ".git/refs/issues/#{branch[1]}", number if branch
 end
 
 ISSUE_PATTERN = /^\[#(\d+)\]/
 
 msg = File.read(ARGV[0])
 
-m = msg.match ISSUE_PATTERN
-if !m.nil?
-  cache_issue m[1]
+issue_match = msg.match ISSUE_PATTERN
+unless issue_match.nil?
+  cache_issue issue_match[1]
   exit 0
 end
 
 issue = File.read('.git/ISSUE') if File.exists? '.git/ISSUE'
 
-File.write(ARGV[0], "[##{issue}] #{msg}") && exit(0) if !issue.nil?
+File.write(ARGV[0], "[##{issue}] #{msg}") && exit(0) unless issue.nil?
 
 puts "Do you want to specify issue? Enter a number if yes or anything else if no."
 
